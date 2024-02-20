8280b95893b5ed1d4d31643d3d13921720f73554
redlik <ralphr@outlook.com>
2024-02-20 07:12:22 +0000

    Initial WT plugin file

---
+++ wetravel-integration.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       WeTravel Integration
+ * Plugin URI:        http://collage.ie
+ * Description:       WeTravel Integration plugin.
+ * Version:           1.0.0
+ * Author:            Rafo
+ * Author URI:        http://example.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wetravel-integration
+ * Domain Path:       /languages
+ */
